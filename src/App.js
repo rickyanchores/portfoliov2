e15d2ebe41459f68d6893c7c8b0cb47397184743
RickyAnchores <ricky23anchores@gmail.com>
2021-06-29 20:02:23 +0100

    added Home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 // IMPORTS COMPONENTS
 import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
       <Nav />
+      <Home />
     </div>
   );
 }
