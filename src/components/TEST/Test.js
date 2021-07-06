44d817e442fff1e585fcaf326be3462375eb2c3e
RickyAnchores <ricky23anchores@gmail.com>
2021-07-06 17:23:36 +0100

    Test

--- src/components/TEST/Test.js
+++ src/components/TEST/Test.js
@@ -7,12 +7,22 @@ import { useState} from 'react'
 
 const Test = () => {
 
+    
+
+
     const [name,setName] = useState('Riko')
     const [message,setMessage] = useState('we love coding');
 
     const handleClick = () => {
         setName('Lex');
         setMessage('I hate superman');
+
+        setTimeout(() => {
+            let testSection = document.querySelector('.Test');
+
+            testSection.style.background = "#333";
+            testSection.style.color = "whitesmoke";
+        },3000)
     }
 
 
