81eecebdf671b3f8e6a864e896b440c48a1f6fb0
RickyAnchores <ricky23anchores@gmail.com>
2021-07-07 19:24:48 +0100

    added photo in about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const About = () => {
+
+    const image = 'https://images.unsplash.com/photo-1625320014712-cc333e4e93a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80';
+
     return (
         <div className="About">
-            <h1>About</h1>
-            <p>This is ABOUT ME section</p>
-            <h3>Insert Here List of your Tech Stack:</h3>
+            <img className="about-image" src={image} alt=""/>
+            <div className="text">
+                <h1>About</h1>
+                <p>This is ABOUT ME section</p>
+                <h3>Insert Here List of your Tech Stack:</h3>
+            </div>
         </div>
     )
 }
