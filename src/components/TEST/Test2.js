e4307bd1e4bdddc1996c184bfc1407a33dd9f518
RickyAnchores <ricky23anchores@gmail.com>
2021-07-10 17:55:19 +0100

    added useState on Test 2

--- src/components/TEST/Test2.js
+++ src/components/TEST/Test2.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useState} from "react"
 
 const Test2 = () => {
+
+    const [name,setName] = useState('Test2')
+
+    let handleClick = () => {
+        setName('Test second')
+    }
+
     return (
         <div className="Test2">
-            <h3>Test2</h3>
+            <h3>{name}</h3>
+            <p>this is a second test</p>
+            <button onClick={handleClick}>Click Here</button>
         </div>
     )
 }
