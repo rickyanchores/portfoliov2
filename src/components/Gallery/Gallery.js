2e55a5831c74c6c1c97e832bf941b26e8d0964ed
RickyAnchores <ricky23anchores@gmail.com>
2021-07-16 21:24:16 +0100

    gallery update

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -8,11 +8,21 @@ const Gallery = (props) => {
         name: 'Photo One',
         text: 'This is a photo'
     }
+    const photoTwo = {
+        name: 'Photo Two',
+        text: 'This is a photo'
+    }
+    const photoThree = {
+        name: 'Photo Three',
+        text: 'This is a photo'
+    }
 
     return (
         <div className="Gallery">
             <h1>Gallery</h1>
             <Photo name={photoOne.name} text={photoOne.text}/>
+            <Photo name={photoTwo.name} text={photoTwo.text}/>
+            <Photo name={photoThree.name} text={photoThree.text}/>
         </div>
     )
 }
