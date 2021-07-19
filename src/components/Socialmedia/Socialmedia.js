5d600a2883ce45809bf1c98a5e5ecbee0a4025f3
RickyAnchores <ricky23anchores@gmail.com>
2021-07-19 19:51:29 +0100

    edited socialmedia

--- src/components/Socialmedia/Socialmedia.js
+++ src/components/Socialmedia/Socialmedia.js
@@ -3,8 +3,16 @@ import SocialmediaIcon from './SocialmediaIcon'
 
 const Socialmedia = (props) => {
 
-    let icon = {
+    let icon1 = {
         name: 'Facebook',
+        img: ''
+    }
+    let icon2 = {
+        name: 'Instagram',
+        img: ''
+    }
+    let icon3 = {
+        name: 'Github',
         img: ''
     }
 
@@ -12,7 +20,9 @@ const Socialmedia = (props) => {
     return (
         <div className="Socialmedia">
             <h3>Social Media</h3>
-            <SocialmediaIcon name={icon.name} icon={icon.img}/>
+            <SocialmediaIcon name={icon1.name} icon={icon1.img} />
+            <SocialmediaIcon name={icon2.name} icon={icon2.img} />
+            <SocialmediaIcon name={icon3.name} icon={icon3.img} />
         </div>
     )
 }
