7d04ecef984dfc5020377ad8364670724fd96e31
RickyAnchores <ricky23anchores@gmail.com>
2021-07-20 20:07:37 +0100

    edit project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,15 +5,15 @@ const Projects = (props) => {
     //JAVASCRIPT HERE 
     const item = [
         {
-            name: 'Title one',
+            name: 'City Pix',
             image: 'https://images.unsplash.com/photo-1617396900799-f4ec2b43c7ae?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8Sjl5clBhSFhSUVl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
         },
         {
-            name: 'Title two',
+            name: 'Zenkana',
             image: 'https://images.unsplash.com/photo-1617360547704-3da8b5363369?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=300&q=60'
         },
         {
-            name: 'Title three',
+            name: 'Kahoii',
             image:'https://images.unsplash.com/photo-1618495293261-3dbcaf9079e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=300&q=60'
         }
     ]
