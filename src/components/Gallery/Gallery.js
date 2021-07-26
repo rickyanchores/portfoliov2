f979394eb0a88363f5f9f7b086839946f912bc3c
RickyAnchores <ricky23anchores@gmail.com>
2021-07-26 18:19:30 +0100

    added photo local src image

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import Photo from './Photo'
-
+//import images
+import projectOne from '../../images/Citypx.jpg';
+import projectTwo from '../../images/rickolor.jpg';
+import projectThree from '../../images/zenkana.jpg';
 
 
 const Gallery = (props) => {
 
     const photoOne = {
         name: 'Photo One',
-        text: 'This is a photo'
+        text: 'This is a photo',
+        image: projectOne
     }
     const photoTwo = {
         name: 'Photo Two',
-        text: 'This is a photo'
+        text: 'This is a photo',
+        image: projectTwo
     }
     const photoThree = {
         name: 'Photo Three',
-        text: 'This is a photo'
+        text: 'This is a photo',
+        image: projectThree
     }
 
     return (
         <div className="Gallery">
             <h1>Gallery</h1>
-            <Photo name={photoOne.name} text={photoOne.text} myImage={photoOne}/>
-            <Photo name={photoTwo.name} text={photoTwo.text}/>
-            <Photo name={photoThree.name} text={photoThree.text}/>
+            <Photo name={photoOne.name} text={photoOne.text} myImage={photoOne.image} />
+            <Photo name={photoTwo.name} text={photoTwo.text} myImage={photoTwo.image} />
+            <Photo name={photoThree.name} text={photoThree.text} myImage={photoThree.image}/>
         </div>
     )
 }
--- src/components/Gallery/Photo.js
+++ src/components/Gallery/Photo.js
@@ -5,6 +5,7 @@ import React from 'react'
 const Photo = (props) => {
     return (
         <div className="Photo">
+            <img src={props.myImage}/>
             <h1>{props.name}</h1>
             <h3>{props.text}</h3>
             <button>Click Here</button>
