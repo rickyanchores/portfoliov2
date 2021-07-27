be7cd7a53cf8f40b52501f4d4dc565bfa1024c3d
RickyAnchores <ricky23anchores@gmail.com>
2021-07-27 18:27:10 +0100

    added images to projects

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -5,18 +5,15 @@ import React from 'react'
 
 const Project = (props) => {
 
-    /*
-    const projOne = [
-        {
-            image: 'src/images/kahoii.jpg'
-        }
-    ]
-    */
+    const handleClick = () => {
+        console.log('CLICKED')
+    }
 
     return (
         <div className="Project">
             <img src={props.image} />
             <h1>{props.name}</h1>
+            <button onClick={ handleClick }><h3>CODE</h3></button>
         </div>
     )
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import Project from './Project';
+//IMPORT IMAGES
+import projectOne from '../../images/Citypx.jpg';
+import projectTwo from '../../images/zenkana.jpg'
+import projectThree from '../../images/kahoii.jpg'
+
+
+
 
 const Projects = (props) => {
     //JAVASCRIPT HERE 
     const item = [
         {
             name: 'City Pix',
-            image: 'https://images.unsplash.com/photo-1617396900799-f4ec2b43c7ae?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDR8Sjl5clBhSFhSUVl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60',
+            image: projectOne,
+            link: "//"
         },
         {
             name: 'Zenkana',
-            image: 'https://images.unsplash.com/photo-1617360547704-3da8b5363369?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=300&q=60'
+            image: projectTwo
         },
         {
             name: 'Kahoii',
-            image:'https://images.unsplash.com/photo-1618495293261-3dbcaf9079e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=300&q=60'
+            image: projectThree
         }
     ]
 
@@ -22,7 +30,7 @@ const Projects = (props) => {
     return (
         <div className="Projects">
             <h1 className="title">Projects</h1>
-            <Project name={item[0].name} image={item[0].image} />
+            <Project name={item[0].name} image={item[0].image} link={item[0].link}/>
             <Project name={item[1].name} image={item[1].image} />
             <Project name={item[2].name} image={item[2].image} />
         </div>
