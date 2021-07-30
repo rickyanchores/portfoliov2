8b51b3cde92d57fa426c1b13737a8660e3854132
RickyAnchores <ricky23anchores@gmail.com>
2021-07-30 18:07:18 +0100

    added styling footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,16 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="Footer">
-            <h1>Created by Ricky A.</h1>
-            <h3>by 2020</h3>
+            <h1>Created by Ricky A</h1>
+            <div className="f-media">
+            <li>
+                <i><h3>Github</h3></i>
+                <i><h3>Instagram</h3></i>
+                <i><h3>Gmail</h3></i>
+            </li>
+            </div>
+            
+            
         </div>
     )
 }
