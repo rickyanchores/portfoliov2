9037597092f83b8a8cf27a32702e10ed89d15b07
RickyAnchores <ricky23anchores@gmail.com>
2021-07-31 23:49:55 +0100

    navbar fixed

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -6,7 +6,7 @@ import React from 'react'
 const Project = (props) => {
 
     const handleClick = () => {
-        console.log('CLICKED')
+        //
     }
 
     return (
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,18 @@ import projectThree from '../../images/kahoii.jpg'
 import projectFour from '../../images/rickolor.jpg'
 import projectFive from '../../images/todoApp.jpg'
 
-
+//BTN direct links
+const button = [
+    {
+        address: "https://www.instagram.com/p/CRrutXerUKNm85rxiUDHGpNPFJVvAKH18-JGx40/"
+    },
+    {
+        address: "https://www.instagram.com/p/CRrutXerUKNm85rxiUDHGpNPFJVvAKH18-JGx40/"
+    },
+    {
+        address: "https://www.instagram.com/p/CRrutXerUKNm85rxiUDHGpNPFJVvAKH18-JGx40/"
+    }
+]
 
 const Projects = (props) => {
     //JAVASCRIPT HERE 
@@ -15,7 +26,7 @@ const Projects = (props) => {
         {
             name: 'City Pix',
             image: projectOne,
-            link: "//"
+            link: "https://www.instagram.com/p/CRrutXerUKNm85rxiUDHGpNPFJVvAKH18-JGx40/"
         },
         {
             name: 'Zenkana',
@@ -39,7 +50,7 @@ const Projects = (props) => {
     return (
         <div className="Projects">
             <h1 className="title">Projects</h1>
-            <Project name={item[0].name} image={item[0].image} />
+            <Project name={item[0].name} image={item[0].image} link={item[0].link}/>
             <Project name={item[1].name} image={item[1].image} />
             <Project name={item[2].name} image={item[2].image} />
             <Project name={item[3].name} image={item[3].image} />
