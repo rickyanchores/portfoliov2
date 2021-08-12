65a6d99c6cead6254bd4f994a88343f465c06090
RickyAnchores <ricky23anchores@gmail.com>
2021-08-12 21:08:54 +0100

    added link on Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
                 <h3>Hi, name is Ricky Anchores</h3> 
                 <h5>Front End Developer</h5>
                 <p>Create and Develop modern and minimalist websites</p>
-                <Link to="" onClick={handleClick}>Check my projects</Link>
+                <Link to="/projects">Check my projects</Link>
             </div>
         </div>
     )
