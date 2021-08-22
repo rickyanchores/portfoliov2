19f9257cde6547456a1142e6789910084e5f2f81
RickyAnchores <ricky23anchores@gmail.com>
2021-08-22 23:44:47 +0100

    added primaryImg

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Projects from '../Projects/Projects';
 //import meSelf from '../../images/me-front-1.jpeg'
-
+import primaryImg from '../../images/Ricky.jpg'
 
 
 const Home = () => {
 
+    
 
     const handleClick = () => {
         // to linkedin
@@ -15,7 +16,7 @@ const Home = () => {
     return (
         <div className="Home">
             <div className="hero-img">
-                <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t31.18172-8/22254883_10210293340394193_3205600766020573504_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=174925&_nc_ohc=xg0lGNfGlHEAX9zOKwX&_nc_ht=scontent.fltn2-1.fna&oh=d340ab6a0394a16d4ebfadbd885145c6&oe=6120794A" alt="me" />
+                <img src={primaryImg} alt="me" />
             </div>
             <div className="hero-title">
                 <h3>Hi, name is Ricky Anchores</h3> 
